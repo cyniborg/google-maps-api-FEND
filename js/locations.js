8f73ff7dd97a1118426edf50db4d59dbf26001a6
Robin <cyniborg@gmail.com>
2017-09-18 22:31:43 +0200

    locations model
    seperation of concerns for better programming

---
+++ js/locations.js
@@ -0,0 +1,40 @@
+// This is a pre made list. Although this could be easily pulled from Google places search or some other API.
+//To keep the app simple I have pre-selected these places of interest in Jaipur, which is my hometown.
+var locations = [{
+    lat: 26.9373166,
+    lng: 75.8154803,
+    name: "Nahargarh Fort",
+  },
+  {
+    lat: 26.9850877,
+    lng: 75.8455928,
+    name: "Jaigarh Fort",
+  },
+  {
+    lat: 26.9854865,
+    lng: 75.8513454,
+    name: "Amber Fort",
+  },
+  {
+    lat: 26.9239363,
+    lng: 75.8267438,
+    name: "Hawa Mahal",
+  },
+  {
+    lat: 26.9040972,
+    lng: 75.79702,
+    name: "Curious Life coffee roasters",
+  },
+  {
+    lat: 26.909924,
+    lng: 75.812962,
+    name: "Hotel Diggi Palace",
+  }
+
+];
+
+// centered the map to the location marker of Nahargarh fort
+var centerLocation = {
+  lat: 26.9373166,
+  lng: 75.8154803
+};
